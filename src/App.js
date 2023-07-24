cb2671a9a18f72dea1cd8ce346c93b71b1926695
Aamir <aamirasaram2107@gmail.com>
2023-07-24 10:57:23 +0200

    Add empty navbar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <>
+     <NavBar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <nav></nav>
+  )
+}
+
+export default NavBar;
