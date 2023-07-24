a49d089394349fb3c502973e9345fed4d7ecffd5
Aamir <aamirasaram2107@gmail.com>
2023-07-24 14:25:35 +0200

    Setup routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <>
      <NavBar />
+     <main>
+        <Routes>
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </main>
     </>
   );
 }
---
+++ src/components/Profile.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div>
+      <h1>My Profile</h1>
+    </div>
+  )
+}
