e02e0a9d12f1358601e967db06d804852c56bc22
Aamir <aamirasaram2107@gmail.com>
2023-07-25 08:55:20 +0200

    Install axios and add fetchMissions method

--- src/redux/missions/missionsSlice.js
+++ src/redux/missions/missionsSlice.js
@@ -1,4 +1,13 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchMissions = createAsyncThunk(
+  'missions/fetchMissions',
+  async () => {
+    const response = await axios.get('https://api.spacexdata.com/v3/missions');
+    return response.data;
+  }
+);
 
 const missionsSlice = createSlice({
   name: 'missions',
