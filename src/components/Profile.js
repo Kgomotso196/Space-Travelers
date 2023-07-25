a7167d7f64d001a1ebe1bed91a2cda146a2b294a
Aamir <aamirasaram2107@gmail.com>
2023-07-25 20:24:15 +0200

    Add styling and leave button to profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import './mission.css'
+import { useSelector, useDispatch } from 'react-redux';
+import './profile.css'
+import { leaveMission } from '../redux/missions/missionsSlice';
 
 const Profile = () => {
   const missions = useSelector((state) => state.missions.missions);
   const filteredMissions = missions.filter((mission) => mission.joined === true);
+  const dispatch = useDispatch();
+
+  const handleClick = (e, { id }) => {
+    e.preventDefault();
+    dispatch(leaveMission({ id }));
+  };
 
   return (
-    <div>
-      <h1>My Missions</h1>
-      <ul>
+    <div id="profile">
+      <h2>My Missions</h2>
+      <ul id="missions">
         {filteredMissions.map((mission) => (
-          <li key={mission.mission_id}>{mission.mission_name}</li>
+          <li key={mission.mission_id}>{mission.mission_name}<button onClick={(e) => { handleClick(e, { id: mission.mission_id }); }}>Leave</button></li>
         ))}
       </ul>
     </div>
