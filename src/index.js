e6e45a026d706d041f02cdda45f9009328773e94
Kgomotso Nacane <kgomotsonacane@gmail.com>
2023-07-25 08:13:44 +0200

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import App from './App';
