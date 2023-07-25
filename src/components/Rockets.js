a4562407930e458fd795f3b3c5df0db75606fa24
Kgomotso Nacane <kgomotsonacane@gmail.com>
2023-07-25 13:12:27 +0200

    Add RocketList component without reservation functionality

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,39 +1,38 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { reserveRocket } from '../redux/rockets/rocketsSlice';
+import { fetchRockets } from '../redux/rockets/rocketsSlice';
 import RocketList from './RocketList';
 
 function Rockets() {
   const dispatch = useDispatch();
-  const { rockets, loading } = useSelector((store) => store.rockets);
+  const rocketsData = useSelector((state) => state.rockets.rockets);
+  const rocketsStatus = useSelector((state) => state.rockets.status);
+  const rocketsError = useSelector((state) => state.rockets.error);
 
   useEffect(() => {
-    if (rockets.length === 0) {
-      dispatch({ type: 'rockets/fetchRockets/pending' });
-    }
-  }, [dispatch, rockets]);
+    dispatch(fetchRockets());
+  }, [dispatch]);
 
-  const clickHandler = (e) => {
-    dispatch(reserveRocket(e.target.id));
-  };
-
-  if (loading === 'loading') {
+  if (rocketsStatus === 'loading') {
     return (
       <h1 style={{ marginLeft: '40px' }}>Loading...</h1>
     );
   }
 
+  if (rocketsError !== null) {
+    return (
+      <h1 style={{ marginLeft: '40px' }}>{rocketsError}</h1>
+    );
+  }
   return (
     <>
-      {rockets.map((item) => (
+      {rocketsData.map((item) => (
         <RocketList
           key={item.id}
           id={item.id}
-          name={item.name}
+          name={item.rocket_name}
           description={item.description}
-          flickrImages={item.image}
-          reserved={item.reserved}
-          onClick={clickHandler}
+          flickrImages={item.flickr_images[0]}
         />
       ))}
     </>
